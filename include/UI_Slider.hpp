d527b06626808bd1012b66b2b9acaa19d75ca7b1
VukMar <vuk.s.maric@gmail.com>
2023-01-12 14:32:34 +0100

    New code for slider class - Works as intended

--- include/UI_Slider.hpp
+++ include/UI_Slider.hpp
@@ -3,6 +3,8 @@
 
 #include <SFML/Graphics.hpp>
 #include <SFML/Audio.hpp>
+#include <iostream>
+#include <cmath>
 
 namespace vui
 {
@@ -12,11 +14,78 @@ class Slider
 
 public:
 
-    Slider();
+Slider();
 
-    ~Slider();
-    
+~Slider();
+
+void create(float lenght, float thickness, sf::Vector2f Position);
+
+void Update(sf::RenderWindow &Window);
+
+void displayTo(sf::RenderWindow &Window);
+
+int getPercentValue();
+
+void setHeadColor(sf::Color Color);
+
+void setAreaColor(sf::Color Color);
+
+void setHeadOutlineThickness(float Thickness);
+
+void setAreaOutlineThickness(float Thickness);
+
+void setHeadOutlineColor(sf::Color Color);
+
+void setAreaOutlineColor(sf::Color Color);
+
+void checkAttributsChanges();
+
+sf::Color getHeadColor();
+
+sf::Color getAreaColor();
+
+float getHeadOutlineThickness();
+
+float getAreaOutlineThickness();
+
+sf::Color getHeadOutlineColor();
+
+sf::Color getAreaOutlineColor();
+
+void setHeadLength(float Length);
+
 private:
+
+bool leftMouseHeld = false;
+bool WFocus = false;
+
+sf::Vector2f CursorPos;
+
+float HeadOutlineThickness = 0;
+float AreaOutlineThickness = 0;
+
+sf::Color AreaColor;
+sf::Color AreaOutlineColor;
+sf::Color HeadColor;
+sf::Color HeadOutlineColor;
+
+float headLength;
+
+sf::RectangleShape SliderArea;
+sf::RectangleShape SliderHead;
+
+int percent = 0;
+
+bool sliderHeadGrabed();
+
+bool mouseOverHead();
+
+bool mouseOverArea();
+
+bool mouseLeftHeld();
+
+bool mouseLeftReleased();
+
 };
 
 }
--- src/UI_Slider.cpp
+++ src/UI_Slider.cpp
@@ -2,5 +2,239 @@
 
 namespace vui
 {
+//Public:
+
+Slider::Slider()
+{
+
+}
+
+Slider::~Slider()
+{
+
+}
+
+void Slider::create(float lenght, float thickness, sf::Vector2f Position)
+{   
+    if(thickness > lenght * 0.3)
+        thickness = lenght * 0.3;
+    
+    headLength = thickness;
+    SliderArea.setSize(sf::Vector2f(lenght, thickness));
+    SliderHead.setSize(sf::Vector2f(thickness,thickness));
+    SliderArea.setPosition(Position);
+    SliderHead.setPosition(Position);
+    SliderHead.setFillColor(sf::Color::Red);
+}
+
+void Slider::Update(sf::RenderWindow &Window)
+{
+    checkAttributsChanges();
+
+    CursorPos = sf::Vector2f(sf::Mouse::getPosition(Window));
+
+    if(!mouseOverArea())
+    {
+        if(mouseLeftHeld())
+            WFocus = true;
+        else
+        {
+            WFocus = false;
+        }
         
+    }
+    else if(mouseOverArea() && !WFocus)
+    {
+        if(sliderHeadGrabed())
+        {
+            CursorPos.y = SliderHead.getPosition().y + SliderHead.getSize().y / 2;
+            sf::Mouse::setPosition(sf::Vector2i(CursorPos), Window);
+            SliderHead.setPosition(CursorPos.x - SliderHead.getSize().x/2, SliderHead.getPosition().y);
+        }
+        else if(mouseLeftReleased())
+        {
+            SliderHead.setPosition(CursorPos.x - SliderHead.getSize().x/2, SliderHead.getPosition().y);
+        }
+    }
+    sf::Vector2f HeadPos = SliderHead.getPosition();
+    sf::Vector2f HeadSize = SliderHead.getSize();
+
+    sf::Vector2f AreaPos = SliderArea.getPosition();
+    sf::Vector2f AreaSize = SliderArea.getSize();
+
+    if(HeadPos.x < AreaPos.x)
+    {
+        SliderHead.setPosition(AreaPos.x,HeadPos.y);
+    }
+    else if(HeadPos.x + HeadSize.x > AreaPos.x + AreaSize.x)
+    {
+        SliderHead.setPosition(AreaPos.x + AreaSize.x - HeadSize.x ,HeadPos.y);
+    }
+    
+    percent = int(ceil(100*((HeadPos.x - AreaPos.x) / (AreaSize.x - HeadSize.x))));
+}
+
+void Slider::displayTo(sf::RenderWindow &Window)
+{
+    Window.draw(SliderArea);
+    Window.draw(SliderHead);
+}
+
+int Slider::getPercentValue()
+{
+    return percent;
+}
+
+void Slider::setHeadColor(sf::Color Color)
+{
+    HeadColor = Color;
+}
+
+void Slider::setAreaColor(sf::Color Color)
+{
+    AreaColor = Color;
+}
+
+void Slider::setHeadOutlineThickness(float Thickness)
+{
+    HeadOutlineThickness = Thickness;
+}
+
+void Slider::setAreaOutlineThickness(float Thickness)
+{
+    AreaOutlineThickness = Thickness;
+}
+
+void Slider::setHeadOutlineColor(sf::Color Color)
+{
+    HeadOutlineColor = Color;
+}
+
+void Slider::setAreaOutlineColor(sf::Color Color)
+{
+    AreaOutlineColor = Color;
+}
+
+void Slider::setHeadLength(float Length)
+{
+    headLength = Length;
+}
+
+void Slider::checkAttributsChanges()
+{
+    //Slider head check
+    if(SliderHead.getFillColor() != HeadColor)
+        SliderHead.setFillColor(HeadColor);
+    if(SliderHead.getOutlineColor() != HeadOutlineColor)
+        SliderHead.setOutlineColor(HeadOutlineColor);
+    if(SliderHead.getOutlineThickness() != HeadOutlineThickness)
+        SliderHead.setOutlineThickness(HeadOutlineThickness);
+    if(headLength != SliderHead.getSize().x)
+    {
+        if(headLength > SliderArea.getSize().x/4)
+        {
+            headLength = SliderArea.getSize().x/4;
+        }
+        SliderHead.setSize(sf::Vector2f(headLength, SliderHead.getSize().y));
+    }
+
+    //Slider area check
+    if(SliderArea.getFillColor() != AreaColor)
+        SliderArea.setFillColor(AreaColor);
+    if(SliderArea.getOutlineColor() != AreaOutlineColor)
+        SliderArea.setOutlineColor(AreaOutlineColor);
+    if(SliderArea.getOutlineThickness() != AreaOutlineThickness)
+        SliderArea.setOutlineThickness(AreaOutlineThickness);
+}
+
+sf::Color Slider::getHeadColor()
+{
+    return HeadColor;
+}
+
+sf::Color Slider::getAreaColor()
+{
+    return AreaColor;
+}
+
+float Slider::getHeadOutlineThickness()
+{
+    return HeadOutlineThickness;
+}
+
+float Slider::getAreaOutlineThickness()
+{
+    return AreaOutlineThickness;
+}
+
+sf::Color Slider::getHeadOutlineColor()
+{
+    return HeadOutlineColor;
+}
+
+sf::Color Slider::getAreaOutlineColor()
+{
+    return AreaOutlineColor;
+}
+
+//Private:
+
+bool Slider::sliderHeadGrabed()
+{
+    if(mouseOverHead())
+    {
+        if(mouseLeftHeld())
+            return true;
+    }
+    return false;
+}
+
+bool Slider::mouseOverHead()
+{
+    sf::Vector2f HeadPos = SliderHead.getPosition();
+    sf::Vector2f HeadSize = SliderHead.getSize();
+    
+    if(CursorPos.x > HeadPos.x && CursorPos.x < HeadPos.x + HeadSize.x
+    && CursorPos.y > HeadPos.y && CursorPos.y < HeadPos.y + HeadSize.y)
+        return true;
+    else
+        return false;
+}
+
+bool Slider::mouseOverArea()
+{
+    sf::Vector2f AreaPos = SliderArea.getPosition();
+    sf::Vector2f AreaSize = SliderArea.getSize();
+
+    if(CursorPos.x > AreaPos.x && CursorPos.x < AreaPos.x + AreaSize.x
+    && CursorPos.y > AreaPos.y && CursorPos.y < AreaPos.y + AreaSize.y)
+        return true;
+    else
+        return false;
+}
+
+bool Slider::mouseLeftHeld()
+{
+    if(sf::Mouse::isButtonPressed(sf::Mouse::Left))
+    {
+        return true;
+    }
+    return false;
+}
+
+bool Slider::mouseLeftReleased()
+{
+    if(mouseLeftHeld())
+    {
+        leftMouseHeld = true;
+        return false;
+    }
+    else if(leftMouseHeld)
+    {
+        leftMouseHeld = false;
+        return true;
+    }
+    return false;
+}
+
 }
