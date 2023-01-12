3553d0309d7089a32df68c7d2d8d05314a61ef72
VukMar <vuk.s.maric@gmail.com>
2023-01-12 01:27:10 +0100

    Deleted UI_Slider code. Rewrite needed!!

--- include/UI_Slider.hpp
+++ include/UI_Slider.hpp
@@ -7,12 +7,6 @@
 namespace vui
 {
 
-enum Orientation
-{
-    Horizontal = 1,
-    Vertical = 2
-};
-
 class Slider
 {
 
@@ -22,48 +16,7 @@ public:
 
     ~Slider();
     
-    //////////////////////////////////////////////////////////////
-    /// @brief Creates a slider
-    /// @param Position Position of the Slider
-    /// @param Size Size.x is Thickness and Size.y is Length
-    /// @param Style Has two Styles : "Basic" or "Rounded"
-    /// @param Orientation Use vui::Orientation
-    //////////////////////////////////////////////////////////////
-    void Create(sf::Vector2f Position, sf::Vector2f Size, std::string Style, int Orientation);
-
-    //////////////////////////////////////////////////////////////
-    /// @brief Displays Slider to sf::RenderWindow
-    /// @param win sf::RenderWindow that will display the Slider
-    //////////////////////////////////////////////////////////////
-    void displayTo(sf::RenderWindow &win);
-
 private:
-    
-    sf::Vector2f Pos;
-    sf::Vector2f SliderSize;
-    
-    std::string SliderStyle;
-
-    bool SliderCreated = false;
-
-    bool isHorizontal = false;
-    bool isVertical = false;
-    bool EndPieces = false;
-    bool ShowIndicator = true;
-//Basic:
-    sf::RectangleShape SliderArea;
-    sf::RectangleShape Indicator;
-    sf::RectangleShape SliderHead1;
-//Rounded:
-    sf::CircleShape SliderHead2;
-    sf::CircleShape EndPiece1_c;
-    sf::CircleShape EndPiece2_c;
-
-    bool mouseLeftCilckReleased();
-    
-    void drawBasic(sf::RenderWindow &win);
-
-    void drawRounded(sf::RenderWindow &win);
 };
 
 }
